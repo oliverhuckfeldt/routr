ba7d3a89a020913ffb718ab4bbd845a4156f9c58
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2021-12-27 03:55:47 +0100

    Changing to popstate event. Simple API for routing.

--- routr.js
+++ routr.js
@@ -1,7 +1,34 @@
 class Routr {
+
+    #routes = [];
+
     constructor() {
-        window.addEventListener('hashchange', () => {
-            console.log('New Route: ', window.location.hash)
+        window.addEventListener('popstate', (event) => {
+            if (!event.state)
+                return;
+
+            for (const i in this.#routes) {
+                if (this.#routes[i].route === event.state.route) {
+                    this.#routes[i].callback.call();
+                }
+            }
         });
+    }
+
+    register(route, callback) {
+        this.#routes.push({route, callback});
+    }
+
+    navTo (route) {
+        for (const i in this.#routes) {
+            if (this.#routes[i].route === route) {
+                this.#routes[i].callback.call();
+                window.history.pushState({route}, '', route)
+
+                return;
+            }
+        }
+
+        throw new Error('Route is not registered.')
     }
 }
